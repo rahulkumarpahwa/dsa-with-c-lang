9c21124cc2d55a2aadc931c16e01a13601310e74
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-10-04 18:07:01 +0530

    created deletion case of linked list

--- deletion_linked_list.c
+++ deletion_linked_list.c
@@ -15,16 +15,40 @@ void linkedListTraversal( struct Node * ptr ){
 
 // case 1: deletion at the start 
 struct Node * deletionAtStart(struct Node * head){
+  struct Node * p = head;
   head = head->next;
+  free(p);
   return head;
 }
 
 // case 2: deletion in between 
 struct Node * deletionInBetween(struct Node * head , int index){
-
-
+struct Node * p = head;
+int j=0;
+while (j < index-1){
+p= p->next;
+j++;
+}
+struct Node * q = p->next;
+p-> next = q->next;
+free(q);
+return head;
 }
 
+
+// case 3: delete at the end.
+struct Node * deleteAtEnd(struct Node * head){
+  struct Node *p = head;
+  struct Node *q = head->next;
+
+  while (q->next != NULL){
+    p = p->next;
+    q = q->next;
+  }
+  p->next = NULL;
+  free(q);
+  return head;
+}
 
 
 void main(){
@@ -44,10 +68,13 @@ third -> data = 22;
 third -> next = NULL;
 
 // case 1: deletion at the start 
-head = deletionAtStart(head);
+// head = deletionAtStart(head);
 
 // case 2: deletion in between 
-head = deletionInBetween(head , 2); // head and index at which we want delete.
+// head = deletionInBetween(head , 2); // head and index at which we want delete starting from 0.
+
+// case 3: delete at the end.
+head = deleteAtEnd(head);
 
 linkedListTraversal(head);
 }
