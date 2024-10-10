c942182e29242de0b1e2594bb812a7450de8c02e
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2024-10-10 22:07:52 +0530

    stack with isEmpty and isFull functions

---
+++ stack.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Stack
+{
+    int size;
+    int top;
+    int *arr;
+};
+
+int isEmpty(struct Stack *ptr)
+{
+    if (ptr->top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int isFull(struct Stack *ptr)
+{
+    if (ptr->top == ptr->size)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+void main()
+{
+    struct Stack *s = (struct Stack *)malloc(sizeof(struct Stack));
+    s->size = 80;
+    s->top = -1;
+    s->arr = (int *)malloc(sizeof(int) * s->size);
+
+    if (isEmpty(s))
+    {
+        printf("stack is underflow\n");
+    }
+    else
+    {
+        printf("stack is not empty\n");
+    }
+
+    if (isFull(s))
+    {
+        printf("stack is overflow\n");
+    }
+    else
+    {
+        printf("stack is not full\n");
+    }
+}
