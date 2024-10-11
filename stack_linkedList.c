f3115cd2c2ceb01268155b41d7ddde4f0b83dfe2
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2024-10-12 00:53:56 +0530

    stack with linked list and isEmpty and isFull Method

--- stack_linkedList.c
+++ stack_linkedList.c
@@ -7,8 +7,41 @@ struct Node
     struct Node *next;
 };
 
+int isEmpty(struct Node *top)
+{
+    if (top == NULL)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int isFull()
+{
+    struct Node *n = (struct Node *)malloc(sizeof(struct Node));
+    if (n == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
 void main()
 {
+    struct Node * top = (struct Node *)malloc(sizeof(struct Node));
+    top = NULL;
 
-    
+    if (isEmpty(top))
+    {
+        printf("Stack is underflow");
+    }
+    else if (isFull())
+    {
+        printf("Stack is overflow");
+    } else {
+        printf("Stack is Not Empty Or Full");
+    }
 }
